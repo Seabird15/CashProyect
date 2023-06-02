b148e13cb71f891398d1e2be305225d724ead4df
Daniela Lizama <2b-0005@MacBook-Pro-de-IT.local>
2023-06-01 20:04:58 -0400

    header

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <!--Componente layout es para acomodar los elementos en pantalla-->
+    <Layout>
+        <template #header>
+            <Header></Header>
+        </template>
+    </Layout>
+</template>
+
+<script>
+import Layout from "@/components/Layout.vue"
+import Header from "@/components/Header.vue"
+
+export default {
+    components: {
+        Layout,
+        Header,
+    }
+}
+</script>
